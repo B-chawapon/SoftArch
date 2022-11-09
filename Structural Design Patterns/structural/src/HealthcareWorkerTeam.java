ba26015757c758be22bde8db37cad6e56c240a92
Chawapon <68158123+B-chawapon@users.noreply.github.com>
2022-11-09 17:57:21 +0700

    Add files via upload

---
+++ Structural Design Patterns/structural/src/HealthcareWorkerTeam.java
@@ -0,0 +1,43 @@
+package src;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HealthcareWorkerTeam implements HealthcareServiceable {
+    private Set<HealthcareServiceable> members;
+
+    public HealthcareWorkerTeam() {
+        this.members = new HashSet<HealthcareServiceable>();
+
+    }
+
+    public void addMember(HealthcareServiceable healthcareServiceable) {
+        this.members.add(healthcareServiceable);
+    }
+
+    public void removeMember(HealthcareServiceable healthcareServiceable) {
+        this.members.remove(healthcareServiceable);
+
+    }
+
+    @Override
+    public void service() {
+        Iterator<HealthcareServiceable> itr = this.members.iterator();
+        while (itr.hasNext()) {
+            itr.next().service();
+        }
+    }
+
+    @Override
+    public double getPrice() {
+        Iterator<HealthcareServiceable> itr = this.members.iterator();
+        double totalPrice = 0;
+        while (itr.hasNext()) {
+            totalPrice += itr.next().getPrice();
+        }
+        return totalPrice;
+
+    }
+
+}
